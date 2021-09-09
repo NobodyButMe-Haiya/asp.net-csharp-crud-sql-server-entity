335c06557eb622e0e9b110f3b836b7ae085a3e6d
nobody but me <38270090+NobodyButMe-Haiya@users.noreply.github.com>
2021-09-09 13:19:03 +0800

    * nlog.config: * InjectLog.cs:
    * .DS_Store: Update log ?

---
+++ SqlServerDockerCSharp/InjectLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NLog;
+
+namespace SqlServerDockerCSharp
+{
+    public class InjectLog
+    {
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void InjectLogInfo(String text)
+        {
+            logger.Info(text);
+
+        }
+        public static void InjectLogDebug(String text)
+        {
+            logger.Debug(text);
+
+        }
+        public static void InjectLogError(String text)
+        {
+            logger.Error(text);
+
+        }
+    }
+}
